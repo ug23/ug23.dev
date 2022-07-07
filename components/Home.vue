bcf2fe2d22033ccd7ceb19e97244ee0f2873bec7
Yuji Imagawa <using923@gmail.com>
2022-07-07 10:20:34 +0900

    feat: homeコンポーネント追加

---
+++ components/Home.vue
@@ -0,0 +1,18 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtHome'
+}
+</script>
+
+<style lang="scss">
+$color-darkBlue: rgba(8, 42, 64, 1);
+$color-green: rgba(28, 140, 140, 1);
+$color-lightGreen: rgba(132, 191, 158, 1);
+$color-yellow: rgba(242, 220, 155, 1);
+$color-red: rgba(242, 113, 102, 1);
+</style>
