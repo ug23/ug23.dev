eb76916ddf01ec0655b1121d6970e44f1adf0ef8
Yuji Imagawa <imagawa@bengo4.com>
2022-07-13 12:03:22 +0900

    feat: 個人のページを追加

---
+++ components/PortfolioSoloDescription.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="mt-4">
+    <h2>基本情報</h2>
+    <ul>
+      <li>名前: 今川裕士（いまがわゆうじ）</li>
+      <li>生年月日: 1991/09/23 おとめ座(29°)</li>
+      <li>住所: 東京都品川区</li>
+      <li>出身: 山形県山形市</li>
+    </ul>
+    <h2>お仕事について</h2>
+    <p>
+      サーバサイドを中心とした開発業務のほか、<br />
+      HubspotやSalesforceなどのCRMの導入支援・カスタマイズ開発業務をメインにお仕事させていただいております。
+    </p>
+    <p>
+      データの加工から可視化までを自動化させて、それがちゃんとしくみとして動いているところを見るのが好きです。
+    </p>
+
+    <h2>お仕事のご依頼について</h2>
+    <p>以下のフォームまたは using923+solo◇gmail.com （◇→@）へどうぞ</p>
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSe7I-DDUelvXtoJo9aUzeQ3tZd7fyYaCKjW_4Cv8ZzrfIECnA/viewform?embedded=true"
+      width="640"
+      height="960"
+      frameborder="1"
+      marginheight="20"
+      marginwidth="20"
+      scrolling="no"
+      >読み込んでいます…</iframe
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioDescription',
+}
+</script>
+
+<style lang="scss">
+.navbar {
+  background-color: $color-green;
+  color: $color-white;
+  font-size: 32px;
+  padding: 0 32px;
+}
+
+.img-wrapper {
+  display: float;
+}
+
+.photo {
+  width: 320px;
+}
+</style>
---
+++ pages/solo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <PortfolioWrapper>
+      <PortfolioImages />
+      <PortfolioSoloDescription />
+    </PortfolioWrapper>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+// @ts-ignore
+export default Vue.extend({
+  name: 'Solo',
+})
+</script>
